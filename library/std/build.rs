141fe58c7fcb249c3b66904b3326610c328b854e
li shaowen <litony0426@gmail.com>
2024-06-09 08:08:34 +0000

    add mimalloc-rs as a dep to std

--- library/std/build.rs
+++ library/std/build.rs
@@ -1,4 +1,59 @@
+#![allow(dead_code)]
+#![allow(unused_imports)]
 use std::env;
+use std::path::{Path, PathBuf};
+use std::process::Command;
+
+const MIMALLOC_ROOT: &str = "/home/swli/inprocess/mimalloc";
+const MIMALLOC_SRC: &str = "/home/swli/inprocess/mimalloc/src";
+
+fn rerun_if_changed_anything_in_dir(dir: &Path) {
+    let mut stack = dir
+        .read_dir()
+        .unwrap()
+        .map(|e| e.unwrap())
+        .filter(|e| &*e.file_name() != ".git")
+        .collect::<Vec<_>>();
+    while let Some(entry) = stack.pop() {
+        let path = entry.path();
+        if entry.file_type().unwrap().is_dir() {
+            stack.extend(path.read_dir().unwrap().map(|e| e.unwrap()));
+        } else {
+            println!("cargo:rerun-if-changed={}", path.display());
+        }
+    }
+}
+
+fn build_mimalloc() {
+    let mut out_dir = PathBuf::new();
+    out_dir.push(env::var("OUT_DIR").unwrap());
+    let build_dir = out_dir.join("mimalloc/build");
+    let install_dir = out_dir.join("mimalloc/install");
+    Command::new("cmake")
+        .args(["-S", MIMALLOC_ROOT])
+        .args(["-B", build_dir.to_str().unwrap()])
+        .args([
+            "-DMI_SECURE=ON",
+            "-DMI_BUILD_SHARED=OFF",
+            "-DMI_BUILD_OBJECT=ON",
+            "-DMI_BUILD_TESTS=OFF",
+            "-DMI_INSTALL_TOPLEVEL=ON",
+            "-DCMAKE_BUILD_TYPE=Release",
+        ])
+        .arg(format!("-DCMAKE_INSTALL_PREFIX={}", install_dir.to_str().unwrap()))
+        .output()
+        .expect("mimalloc cmake configure failed");
+
+    Command::new("make")
+        .args(["-C", build_dir.to_str().unwrap()])
+        .arg("install")
+        .output()
+        .expect("make mimalloc filed");
+
+    let lib_dir = install_dir.join("lib");
+    println!("cargo::rustc-link-search=native={}", lib_dir.to_str().unwrap());
+    println!("cargo:rustc-link-lib=static=mimalloc");
+}
 
 fn main() {
     println!("cargo:rerun-if-changed=build.rs");
@@ -62,4 +117,11 @@ fn main() {
     }
     println!("cargo:rustc-env=STD_ENV_ARCH={}", env::var("CARGO_CFG_TARGET_ARCH").unwrap());
     println!("cargo:rustc-cfg=backtrace_in_libstd");
+
+    {
+        //compiler mimalloc
+
+        rerun_if_changed_anything_in_dir(Path::new(MIMALLOC_SRC));
+        // build_mimalloc();
+    }
 }
