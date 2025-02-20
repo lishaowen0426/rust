53b76c857883e6499eefb2810e313bca687edfec
Li Shaowen <litony0426@gmail.com>
2025-02-20 16:23:00 +0900

    update int-to-ptr-check

--- compiler/rustc_mir_transform/src/int_to_ptr.rs
+++ compiler/rustc_mir_transform/src/int_to_ptr.rs
@@ -6,6 +6,7 @@ use std::path::PathBuf;
 use rustc_middle::mir::visit::Visitor;
 use rustc_middle::mir::*;
 use rustc_middle::ty::TyCtxt;
+use tracing::{info, instrument};
 pub(super) struct IntToPtr;
 
 impl<'tcx> crate::MirPass<'tcx> for IntToPtr {
@@ -13,18 +14,19 @@ impl<'tcx> crate::MirPass<'tcx> for IntToPtr {
         sess.opts.unstable_opts.int_to_ptr_check
     }
 
+    #[instrument(level = "info", skip_all, name = "int_to_ptr_run")]
     fn run_pass(&self, tcx: TyCtxt<'tcx>, body: &mut Body<'tcx>) {
         let mut output = PathBuf::new();
         output
             .push(tcx.sess.io.output_dir.as_ref().unwrap_or(env::current_dir().as_ref().unwrap()));
         output.push("int_to_ptr");
-        let mut file = File::create(output).unwrap();
         let mut vis = IntToPtrVisitor { results: Vec::new() };
         vis.visit_body(body);
         if vis.results.is_empty() {
             return;
         }
-        eprintln!("Int to ptr found:");
+        let mut file = File::create(output).unwrap();
+        info!("Int to ptr found: {:?}", vis.results);
         for location in vis.results {
             let src = body.source_info(location);
             let stmt = body.stmt_at(location);
